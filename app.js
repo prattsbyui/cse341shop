a498c1684ba23d4d10b7311e228e2762c2b36df9
prattsbyui <pratts@byui.edu>
2021-10-18 10:50:46 -0500

    make it better

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found', path: 'error' });
 });
 
-listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT);
 
 
 
